5747a9f8af59ea0b3a38d612caea25ee08fc906c
Alvaro <alvarocastillosempere@gmail.com>
2023-10-24 08:39:20 +0200

    feat: card container

---
+++ src/app/shared/theme/components/card-container/card-container.component.ts
@@ -0,0 +1,26 @@
+import {Component, Input} from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-card-container',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './card-container.component.html',
+  styleUrls: ['./card-container.component.scss']
+})
+export class CardContainerComponent {
+
+  @Input() title: string | null = null
+  @Input() subtitle: string | null = null
+  @Input() marginTop: string = '20px'
+  @Input() backgroundColor: string = '#F5F5F5'
+  @Input() set backgroundImage(value: string) {
+    this._backgroundImage = value
+  }
+
+  private _backgroundImage: string = 'none'
+
+  public get backgroundImage(): string {
+    return `url(${this._backgroundImage})`
+  }
+}
