d8d9a390b76a3bb72d59d59d199799adc30beaae
Alvaro <alvarocastillosempere@gmail.com>
2023-10-30 08:30:15 +0100

    chore: remove unused modules and components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './core/components/header/header.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [HeaderComponent, RouterOutlet],
+  imports: [RouterOutlet],
 })
 export class AppComponent {
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,7 @@ import {LayoutComponent} from "./shared/theme/layout/layout.component";
 
 export const routes: Routes = [
   { path: '', component: LayoutComponent, children: [
-      {path: '', redirectTo: 'home', pathMatch: 'full'},
-      {path: 'home', loadComponent: () => import('./home/home.component')},
-      {path: 'feature', loadComponent: () => import('./feature/feature.component')},
+      {path: '**', redirectTo: '', pathMatch: 'full'},
+      {path: '', loadComponent: () => import('./home/home.component')},
     ] }
 ];
